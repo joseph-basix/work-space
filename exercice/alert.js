c511d51ceee9ce1a51efef786134899a7a2a61fb
josephbasamuk <josephnyandu@icloud.com>
2023-03-31 17:51:11 +0100

    dd

---
+++ exercice/alert.js
@@ -0,0 +1,46 @@
+
+//document.getElementById("xy").innerHTML = ("Une simple page HTML");
+
+
+
+
+
+// syntaxe conditionnelle
+
+/* let age = 100;
+
+if (age >= 20){
+
+console.log(`A ${age} ans, vous êtes majeure.`);
+}
+
+else{
+    console.log(`A ${age} ans, vous êtes encore mineur.`)
+} */
+
+
+
+
+
+
+
+
+// operateure ternaire (Un autre facon d'ecrire ce code ci-haut)
+
+/*let age = 50;
+
+
+age >= 30 ? console.log(`A ${age} ans, vous êtes majeure.`) : console.log(`A ${age} ans, vous êtes encore mineur.`);*/
+
+let a = 1;
+
+ while(a < 100){
+
+    console.log(`la valeur de la variable a est ${a}.`);
+    a++;
+
+     }
+    console.log("Boucle terminer")
+
+
+
