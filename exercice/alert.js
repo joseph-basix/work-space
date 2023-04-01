506833aed24f88bf183c6207db8c19ad08c3b1b8
josephbasamuk <josephnyandu@icloud.com>
2023-04-01 01:40:37 +0100

    gg

--- exercice/alert.js
+++ exercice/alert.js
@@ -1,14 +1,16 @@
 
 //document.getElementById("xy").innerHTML = ("Une simple page HTML");
 
- let user = 40;
 
 
 
- if (user >= 18){
-console.log(`A ${user} ans vous etes autorise a visiter notre site`)
 
- } else{
-    console.log(` les mineur ne sont pas autorise a visiter notre site`)
- }
 
+let Eco_lecsmo = {
+
+    Nomb_resprof : 23,
+    Nmbr_eclass : 10,
+    Nom_prof : "mattieu,marc,jul ,jeans,paule,elyse",
+}
+
+console.log(Eco_lecsmo.Nom_prof);
