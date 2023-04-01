4d01ddd73e6e629c2274a93d029a9e70bda8cd1e
josephbasamuk <josephnyandu@icloud.com>
2023-04-01 01:32:15 +0100

    gg

--- exercice/alert.js
+++ exercice/alert.js
@@ -1,60 +1,14 @@
 
 //document.getElementById("xy").innerHTML = ("Une simple page HTML");
 
+ let user = 40;
 
 
 
+ if (user >= 18){
+console.log(`A ${user} ans vous etes autorise a visiter notre site`)
 
-// syntaxe conditionnelle
-
-/* let age = 100;
-
-if (age >= 20){
-
-console.log(`A ${age} ans, vous êtes majeure.`);
-}
-
-else{
-    console.log(`A ${age} ans, vous êtes encore mineur.`)
-} */
-
-
-
-
-
-
-
-
-// operateure ternaire (Un autre facon d'ecrire ce code ci-haut)
-
-/*let age = 50;
-
-
-age >= 30 ? console.log(`A ${age} ans, vous êtes majeure.`) : console.log(`A ${age} ans, vous êtes encore mineur.`);*/
-
-
-
-/*let a = 1;
-
- while(a < 10){
-
-    console.log(`la valeur de la variable a est ${a}.`);
-    a++;
-
-     }
-    console.log("Boucle terminer")  */
-
-
-let Eco_lecsmo = {
-
-    Nomb_resprof : 23,
-    Nmbr_eclass : 10,
-    Nom_prof : "mattieu,marc,jul ,jeans,paule,elyse"
-
-
-
-}
-  
-
-console.log(Eco_lecsmo.Nom_prof);
+ } else{
+    console.log(` les mineur ne sont pas autorise a visiter notre site`)
+ }
 
