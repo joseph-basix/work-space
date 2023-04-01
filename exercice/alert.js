21755eb4defa50969adec9bebb23fad838a40487
josephbasamuk <josephnyandu@icloud.com>
2023-04-01 17:32:44 +0100

    as

--- exercice/alert.js
+++ exercice/alert.js
@@ -1,14 +1,3 @@
-
-//document.getElementById("xy").innerHTML = ("Une simple page HTML");
-
+document.getElementById("xy").innerHTML = ("Une simple page HTML");
 
 
-let a = 10;
-
-while(a <100){
-
-console.log(`la valeur de la variable a est ${a}.`)
-a++
-
-}
-console.log("boucle terminer");
