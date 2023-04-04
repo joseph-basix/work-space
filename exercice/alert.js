7de5e88b7ed681766056b2e93e6d7ce9b64c99f6
josephbasamuk <josephnyandu@icloud.com>
2023-04-04 22:15:37 +0100

    1

--- exercice/alert.js
+++ exercice/alert.js
@@ -1,3 +1,10 @@
-document.getElementById("xy").innerHTML = ("Une simple page HTML");
+//document.getElementById("xy").innerHTML = ("Une simple page HTML");
 
 
+let consoleAleatoire = function () {
+    let nombreAleatoire = Math.floor( Math.randoom() * 100 ) + 1;
+    console.log(nombreAleatoire);
+};
+
+consoleAleatoire();
+consoleAleatoire();
