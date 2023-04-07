8e17974069680c670bafd0fc66484a3234b80934
josephbasamuk <josephnyandu@icloud.com>
2023-04-07 01:39:31 +0100

    yous

--- exercice/alert.js
+++ exercice/alert.js
@@ -1,36 +1,3 @@
 document.getElementById("xy").innerHTML = ("Une simple page HTML");
 
 
-
-
-
-
-// METHODE  POUR DECLARER UNE FONCTION (1)
-
-
-/* let a= 2, b = 3 , result;
-
-function addNumber(){
-result = a+b;
-
-console.log(result);
-}
-
-addNumber(); */
-
-
-// METHODE  POUR DECLARER UNE FONCTION (2)
-
-
-let a= 2, b = 3 , result;
-
-let addNumber = function(){
-
-    result = a+b;
-    console.log(result)
-}
-
-addNumber();
-
-
-//METHODE  POUR ENVOYER DES DONNEE DANS UNE FONCTION
