fa98e59be7764c4d5afa4c44ffbc3cf674b46a5b
josephbasamuk <josephnyandu@icloud.com>
2023-04-18 23:21:49 +0100

    2

--- newFormation/exemple.js
+++ newFormation/exemple.js
@@ -5,11 +5,33 @@
 
 
 
-let  num1 = 4, num2 = 5, result ;
+// let  num1 = 4, num2 = 5, result ;
 
-result =  (num1*num2) - 5;
-result ++;
-// console.log(result);
+// result =  (num1*num2) - 5;
+// result ++;
+// // console.log(result);
 
 
-console.log("voila mes points" + '  ' + result + 10 + " Dieu soit louer ");
+// console.log("voila mes points" + '  ' + result + 10 + " Dieu soit louer ");
+
+
+// confirm('hello word !')
+
+// if (confirm('souhaitez-vous confirmer')){
+
+//     alert('ok');
+// } 
+
+// else{
+
+//     alert('Etez vous sur de supprimer')
+//     alert('Annulation Reussi ');
+// }
+
+
+function direBonjour() {
+    
+   confirm('Bonjour !')
+}
+
+direBonjour()
