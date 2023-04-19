032f39b0e017e724076d7f21130f37183e078a34
josephbasamuk <josephnyandu@icloud.com>
2023-04-19 02:22:02 +0100

    19-1

--- newFormation/exemple.js
+++ newFormation/exemple.js
@@ -1,37 +1,73 @@
-//  const postNom = "nicolas";
-//  const  nom   = "dupont";
 
-//  console.log(" bonjour " + postNom + ' ' + nom + ' ' + '!');
+// let crie;
 
-
-
-// let  num1 = 4, num2 = 5, result ;
-
-// result =  (num1*num2) - 5;
-// result ++;
-// // console.log(result);
-
-
-// console.log("voila mes points" + '  ' + result + 10 + " Dieu soit louer ");
-
-
-// confirm('hello word !')
-
-// if (confirm('souhaitez-vous confirmer')){
-
-//     alert('ok');
-// } 
-
-// else{
-
-//     alert('Etez vous sur de supprimer')
-//     alert('Annulation Reussi ');
-// }
-
-
-function direBonjour() {
+// function crieDeGeurre() {
     
-   confirm('Bonjour !')
-}
+//     crie = "toujours plus fort! "
+//     console.log(crie);
+// }
+// crieDeGeurre();
 
-direBonjour()
+// console.log(crie);
+
+
+// let nombreUn = 4, nombreDeux = 7; // variable a porte globale 
+
+// function addition() {
+//     console.log(nombreUn+nombreDeux);
+// }
+// addition();
+
+
+// let nombreUn = 4, nombreDeux = 7; 
+
+// function addition(nombreA,nombreB) {
+    
+//     let result = nombreA + nombreB;
+//     return result;
+// }
+// console.log(addition(nombreUn,nombreDeux));
+
+
+// Tres importent 
+
+// function division (nombreA,nombreB) {
+//     let result = nombreA * nombreB;
+//     return result;
+// }
+// division(6,8);
+
+// let resulatDivision = 3;
+
+// console.log(division(6,9)+ resulatDivision );
+
+
+// let preparation = 10, cuisson = 15; 
+
+// function cuisinier(nombreDeGateaux,minuteDePreparation,minuteDecuisson) {
+    
+//     let resultat = nombreDeGateaux * (minuteDecuisson + minuteDePreparation);
+//     return resultat;
+// }
+// console.log(cuisinier(5,preparation,cuisson));
+
+
+// function prevoirAge() {
+    
+//     let age = prompt("quel est votre age? ");
+//     //age = parseInt(age);
+//     //age = parseFloat(age)
+//     age = Number(age);
+//     alert (" bientot vous aurez " + (age + 1)+ " ans");
+
+    
+// }
+// prevoirAge();
+
+
+// 1.1 LES FONCTION ANONYME 
+
+let fonctionAnonyme = function() {
+    console.log('je suis une fonction anonyme');
+}
+fonctionAnonyme();
